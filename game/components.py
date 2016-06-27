afde8431fa13e736937af0103241275cb221af4e
Philip Brown <philipnbrown@gmail.com>
2016-06-27 15:49:07 -0700

    wrote payoffVec() method which will be useful for equilibrium checking

--- game/components.py
+++ game/components.py
@@ -42,19 +42,20 @@ class Game :
         if not self.n == np.shape(self.pmat)[self.n] :
             raise IndexError('the wrong number of players\' actions are being described')
             
-    def checkActionProfile(self,actionProfile) :
-        for player in range(len(actionProfile)) :
-            stratWeights = np.array(actionProfile[player])
+    def checkActionProfile(self,playersToCheck,actionProfile) :
+        for i in range(len(actionProfile)) :
+            stratWeights = np.array(actionProfile[i])
             # first check that the strategy has the correct length:
-            if not len(self.actions[player]) == len(stratWeights) :
-                raise IndexError('player %d (0-indexed) does not have correct number of action weights specified' % player)
+            if not len(self.actions[playersToCheck[i]]) == len(stratWeights) :
+                raise IndexError('player %d (0-indexed) does not have correct number of action weights specified' % playersToCheck[i])
             # then check that every action weight is nonnegative:
             for action in range(len(stratWeights)) :
                 if not stratWeights[action] >= -myEps :
-                    raise TypeError('action %d for player %d has a negative weight' %(action, player)) #TypeError might not be best exception
+                    raise TypeError('action %d for player %d has a negative weight' %(action, playersToCheck[i])) #TypeError might not be best exception
             # finally check that mixed weights add to 1 (so it's valid probabilities):
             if not np.abs(np.sum(stratWeights)-1) <= myEps :
-                raise TypeError('action weights for player %d do not sum to 1' % player) # again, TypeError might not be best
+#                print(actionProfile)
+                raise TypeError('action weights for player %d do not sum to 1' % playersToCheck[i]) # again, TypeError might not be best
             
     def payoffs(self,playersArg,actionProfile) :
         # returns the payoffs experienced by playersArg given mixed strategies actionProfile
@@ -63,7 +64,7 @@ class Game :
         #    len(actionProfile[i]) = len(self.actions[i])
         # output: array of dimension len(playersArg) of player payoffs
         ap = np.array(actionProfile)
-        self.checkActionProfile(ap)
+        self.checkActionProfile(range(self.n),ap) # check profile for all players
         # first, collapse the payoff matrix using the action profile:
         pmatCollapsed = np.array(self.pmat)
         for player in self.players :
@@ -72,6 +73,31 @@ class Game :
             thisPmat = pmatCollapsed*np.reshape(ap[player],vecShape) # multiply strat weights
             pmatCollapsed = np.sum(thisPmat,0) # sum along dimension 0
         return pmatCollapsed[np.array(playersArg)]
+        
+    def payoffVec(self,playerArg,actionProfileReduced) :
+        # returns a |A_i|-dimensional vector of payoffs for player i=playerArg, given
+        #   mixed strat profile a_{-i}
+        #
+        # First check action profile validity:
+#        apToCheck = np.insert(np.array(actionProfileReduced),playerArg,np.zeros(len(self.actions[playerArg])),axis=0)
+#        print(apToCheck)        
+#        apToCheck[playerArg][0]=1
+        
+        ap = actionProfileReduced.copy()
+        # NEED code here to check if ap has more than 1 dimension; just be safe
+        self.checkActionProfile(np.delete(np.array(range(self.n)),playerArg),ap)
+        #
+        pmatCollapsed = self.pmat[...,playerArg] # get player i's payoffs
+        offset = 0
+        for player in range(self.n-1) : # loop thru all but one player; assume that actionProfileReduced is ordered properly        
+            if player>=playerArg:
+                offset = 1 # leave playerArg alone
+            vecShape = np.ones(len(np.shape(pmatCollapsed)))
+            vecShape[offset] = len(ap[player]) # vecShape ensures that strategy weights are multiplied in properly
+            thisPmat = pmatCollapsed*np.reshape(ap[player],vecShape) # multiply strat weights
+            pmatCollapsed = np.sum(thisPmat,offset) # sum along dimension 0, or 1 if dim 0 corresp to playerArg
+        return pmatCollapsed
+        
 
 
 class GameSimple(Game) :
@@ -91,4 +117,3 @@ class GameSimple(Game) :
             self.actions[player] = np.array(range(actions[player]))
         self.pmat = pmat
         self.checkpmat()
-
